c18e6bb36efd7d36f00cccc9fd36bbce24e783e9
Shetty, Bharatraj <bharatrajs@gmail.com.com>
2017-08-19 13:47:04 +0530

    added launch browser test case

---
+++ src/test/java/com/gittest/bharat/LaunchBrowser.java
@@ -0,0 +1,23 @@
+package com.gittest.bharat;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+WebDriver dr;
+	
+	@Test
+	public void newTest() throws InterruptedException{
+	 	   
+	   System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\geckodriver\\geckodriver.exe");
+	   dr=new FirefoxDriver();
+	   dr.manage().window().maximize();
+	   dr.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	   dr.get("http://album.alexflueras.ro/galleries/landscapes.html");
+	   
+	}
+
+}
